a78cc4974d926e2c27c0aaff4c106d9167889b2d
LynVii <84234492+LynViii@users.noreply.github.com>
2026-09-11 15:04:57 +0800

    feat: polish board rendering and move feedback

--- FiveChess/ChessDraw.cpp
+++ FiveChess/ChessDraw.cpp
@@ -29,15 +29,19 @@ void CChessDraw::InitBoard(CRect rect, UINT uiRows, UINT uiCols, UINT uiBoardWid
     m_uiBoardRows = uiRows;
     m_uiBoardCols = uiCols;
 
-    const int padding = 34;
+    const int padding = 38;
     const int usableWidth = max(1, rect.Width() - padding * 2);
     const int usableHeight = max(1, rect.Height() - padding * 2);
-    const int autoWidth = min(usableWidth / max(1, (int)uiCols - 1), usableHeight / max(1, (int)uiRows - 1));
+    const int autoWidth = min(
+        usableWidth / max(1, (int)uiCols - 1),
+        usableHeight / max(1, (int)uiRows - 1));
 
     m_uiBoardWidth = uiBoardWidth > 0 ? min((int)uiBoardWidth, autoWidth) : autoWidth;
     m_uiBoardWidth = max(10, (int)m_uiBoardWidth);
 
-    m_uiPieceRadius = uiPieceRadius > 0 ? min((int)uiPieceRadius, (int)m_uiBoardWidth / 2 - 2) : (int)m_uiBoardWidth / 2 - 3;
+    m_uiPieceRadius = uiPieceRadius > 0
+        ? min((int)uiPieceRadius, (int)m_uiBoardWidth / 2 - 2)
+        : (int)m_uiBoardWidth / 2 - 3;
     m_uiPieceRadius = max(4, (int)m_uiPieceRadius);
 
     const int boardWidth = m_uiBoardWidth * (m_uiBoardCols - 1);
@@ -72,14 +76,25 @@ void CChessDraw::DrawBackground()
 
     m_pDC->FillSolidRect(m_rcBK, m_crBackground);
 
-    CRect boardCard = m_rcBoard;
-    boardCard.InflateRect(24, 24);
+    CRect shadowCard = m_rcBoard;
+    shadowCard.InflateRect(27, 27);
+    shadowCard.OffsetRect(0, 3);
 
-    CPen borderPen(PS_SOLID, 1, RGB(205, 176, 132));
+    CPen shadowPen(PS_SOLID, 1, RGB(230, 223, 211));
+    CBrush shadowBrush(RGB(230, 223, 211));
+    CPen* oldPen = m_pDC->SelectObject(&shadowPen);
+    CBrush* oldBrush = m_pDC->SelectObject(&shadowBrush);
+    m_pDC->RoundRect(shadowCard, CPoint(18, 18));
+
+    CRect boardCard = m_rcBoard;
+    boardCard.InflateRect(26, 26);
+
+    CPen borderPen(PS_SOLID, 1, RGB(201, 167, 117));
     CBrush boardBrush(m_crBoardSurface);
-    CPen* oldPen = m_pDC->SelectObject(&borderPen);
-    CBrush* oldBrush = m_pDC->SelectObject(&boardBrush);
+    m_pDC->SelectObject(&borderPen);
+    m_pDC->SelectObject(&boardBrush);
     m_pDC->RoundRect(boardCard, CPoint(18, 18));
+
     m_pDC->SelectObject(oldBrush);
     m_pDC->SelectObject(oldPen);
 }
@@ -93,7 +108,6 @@ void CChessDraw::DrawBoard()
 
     CPen gridPen(PS_SOLID, 1, m_crGrid);
     CPen borderPen(PS_SOLID, 2, RGB(68, 54, 39));
-
     CPen* oldPen = m_pDC->SelectObject(&gridPen);
 
     int ptTop = m_rcBoard.top;
@@ -121,6 +135,45 @@ void CChessDraw::DrawBoard()
 
     m_pDC->SelectObject(oldPen);
     DrawSpecialPoints();
+    DrawCoordinateLabels();
+}
+
+void CChessDraw::DrawCoordinateLabels()
+{
+    if (!m_pDC)
+    {
+        return;
+    }
+
+    const int oldBkMode = m_pDC->SetBkMode(TRANSPARENT);
+    const COLORREF oldText = m_pDC->SetTextColor(RGB(112, 86, 56));
+
+    for (UINT x = 0; x < m_uiBoardCols; ++x)
+    {
+        CString label;
+        label.Format(_T("%c"), _T('A') + x);
+        CPoint pt;
+        if (GetPointWithCoordinate(pt, x, 0))
+        {
+            CRect rc(pt.x - 12, m_rcBoard.top - 24, pt.x + 12, m_rcBoard.top - 7);
+            m_pDC->DrawText(label, rc, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+        }
+    }
+
+    for (UINT y = 0; y < m_uiBoardRows; ++y)
+    {
+        CString label;
+        label.Format(_T("%d"), y + 1);
+        CPoint pt;
+        if (GetPointWithCoordinate(pt, 0, y))
+        {
+            CRect rc(m_rcBoard.left - 28, pt.y - 9, m_rcBoard.left - 7, pt.y + 9);
+            m_pDC->DrawText(label, rc, DT_RIGHT | DT_VCENTER | DT_SINGLELINE);
+        }
+    }
+
+    m_pDC->SetTextColor(oldText);
+    m_pDC->SetBkMode(oldBkMode);
 }
 
 BOOL CChessDraw::DrawPiece(UINT uiX, UINT uiY, BOOL bBlack)
@@ -170,6 +223,28 @@ BOOL CChessDraw::DrawPiece(UINT uiX, UINT uiY, BOOL bBlack)
     return TRUE;
 }
 
+void CChessDraw::DrawGhostPiece(UINT uiX, UINT uiY, BOOL bBlack)
+{
+    CPoint pt;
+    if (!m_pDC || !GetPointWithCoordinate(pt, uiX, uiY))
+    {
+        return;
+    }
+
+    const int radius = max(4, (int)m_uiPieceRadius - 2);
+    CRect rc(pt.x - radius, pt.y - radius, pt.x + radius, pt.y + radius);
+
+    const COLORREF border = bBlack ? RGB(104, 111, 107) : RGB(177, 172, 159);
+    const COLORREF fill = bBlack ? RGB(177, 183, 179) : RGB(250, 248, 240);
+    CPen pen(PS_DOT, 1, border);
+    CBrush brush(fill);
+    CPen* oldPen = m_pDC->SelectObject(&pen);
+    CBrush* oldBrush = m_pDC->SelectObject(&brush);
+    m_pDC->Ellipse(rc);
+    m_pDC->SelectObject(oldBrush);
+    m_pDC->SelectObject(oldPen);
+}
+
 void CChessDraw::DrawPieceCur(UINT uiX, UINT uiY)
 {
     CPoint pt;
@@ -189,6 +264,24 @@ void CChessDraw::DrawPieceCur(UINT uiX, UINT uiY)
     m_pDC->SelectObject(oldPen);
 }
 
+void CChessDraw::DrawWinningLine(UINT x1, UINT y1, UINT x2, UINT y2)
+{
+    CPoint start;
+    CPoint end;
+    if (!m_pDC
+        || !GetPointWithCoordinate(start, x1, y1)
+        || !GetPointWithCoordinate(end, x2, y2))
+    {
+        return;
+    }
+
+    CPen linePen(PS_SOLID, 3, m_crAccent);
+    CPen* oldPen = m_pDC->SelectObject(&linePen);
+    m_pDC->MoveTo(start);
+    m_pDC->LineTo(end);
+    m_pDC->SelectObject(oldPen);
+}
+
 void CChessDraw::DrawSpecialPoints()
 {
     if (!m_pDC)
@@ -197,7 +290,7 @@ void CChessDraw::DrawSpecialPoints()
     }
 
     const int points[][2] = {
-        {3, 3}, {11, 3}, {7, 7}, {3, 11}, {11, 11}
+        { 3, 3 }, { 11, 3 }, { 7, 7 }, { 3, 11 }, { 11, 11 }
     };
     const int radius = max(2, (int)m_uiBoardWidth / 10);
 
